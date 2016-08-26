5a4aeb0143ebb204ac535198f3444931989d8402
Chris Hampu <chrishampu@outlook.com>
2016-08-26 09:57:52 -0700

    Update subscription schema
    Update horizon

--- client/components/Profile/ProfileSubscriptionHistory.js
+++ client/components/Profile/ProfileSubscriptionHistory.js
@@ -27,10 +27,9 @@ class SubscriptionHistory extends React.Component {
 
     let history = null;
 
-    if (this.props.subscription.deposit_history && this.props.subscription.withdrawal_history) {
+    if (this.props.subscription.history) {
 
-      history = this.props.subscription.deposit_history
-      .concat(this.props.subscription.withdrawal_history)
+      history = this.props.subscription.history
       .sort((el1, el2) => el2.time - el1.time);
 
       switch(this.state.subFilter) {
