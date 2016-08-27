02c667846d4af7d81f169f206fb47baa5f8eb11f
Chris Hampu <chrishampu@outlook.com>
2016-08-27 08:42:57 -0700

    Improve formatting for numbers

--- client/utilities.js
+++ client/utilities.js
@@ -17,16 +17,16 @@ export function formatNumberUnit(num) {
 
   let suffix = "";
 
-  if (num < -1000000000000 || num > 1000000000000) {
+  if (num <= -1000000000000 || num >= 1000000000000) {
     num /= 1000000000000;
     suffix = "T";
-  } else if (num < -1000000000 || num > 1000000000) {
+  } else if (num <= -1000000000 || num >= 1000000000) {
     num /= 1000000000;
     suffix = "B";
-  } else if (num < -1000000 || num > 1000000) {
+  } else if (num <= -1000000 || num >= 1000000) {
     num /= 1000000;
     suffix = "M";
-  } else if (num < -1000 || num > 1000) {
+  } else if (num <= -1000 || num >= 1000) {
     num /= 1000;
     suffix = "K";
   }
