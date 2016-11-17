6c7b36c747f3bffd80b2de901f07a87aa5e9adc1
Chris Hampu <chrishampu@outlook.com>
2016-11-17 01:27:34 -0800

    Disable tap event plugin

--- client/components/App/AppComponent.js
+++ client/components/App/AppComponent.js
@@ -5,9 +5,10 @@ import store from '../../store';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 
-injectTapEventPlugin();
+// Disable temporarily until package is fixed
+//import injectTapEventPlugin from 'react-tap-event-plugin';
+//injectTapEventPlugin();
 
 const theme = darkBaseTheme;
 
