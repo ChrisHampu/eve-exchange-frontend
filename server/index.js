a42729118ec2d3b1e6d2dcdaa7845f3128a2ca9a
Chris Hampu <chrishampu@outlook.com>
2016-11-18 01:22:50 -0800

    Use better caching on the node server

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import Koa from 'koa';
 import mount from 'koa-mount';
 import convert from 'koa-convert';
 import body from 'koa-better-body';
-import serve from 'koa-static';
+import cached from 'koa-static-cache';
 import historyApiFallback from 'koa-history-api-fallback';
 import chalk from 'chalk';
 import config from './config/environment';
@@ -14,7 +14,10 @@ import DeepstreamAuth from './deepstream_auth';
 
 const httpServer = new Koa();
 httpServer.use(convert(historyApiFallback()));
-httpServer.use(mount('/', serve(path.join(__dirname, '../build'))));
+
+httpServer.use(mount('/', cached(path.join(__dirname, '../build'), {
+  maxAge: 365 * 24 * 60 * 60
+})));
 
 const listener =  httpServer.listen(config.port, () => console.log(chalk.green(`HTTP server is listening on port ${config.port}`)));
 
