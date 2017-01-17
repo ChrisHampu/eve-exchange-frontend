67bf39633887cab9f855e774ff73aecfa2b9e8e2
Chris Hampu <chrishampu@outlook.com>
2017-01-17 13:41:09 -0800

    Improve styling of notification/changelog pages

--- client/components/Feeds/ChangelogComponent.js
+++ client/components/Feeds/ChangelogComponent.js
@@ -3,14 +3,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { prettyDate } from '../../utilities';
 import PaginatedTable from '../UI/PaginatedTable';
+import s from './NotificationsComponent.scss';
+import cx from 'classnames';
 
-import { TableRow, TableRowColumn, TableHeaderColumn } from 'material-ui/Table';
+import { TableHeaderColumn } from 'material-ui/Table';
 
 class Changelog extends React.Component {
 
   render() {
     return (
-      <div style={{display: "flex", height: "100%"}}>
+      <div className={s.root}>
         <PaginatedTable
           headers={[
             <TableHeaderColumn key={0} style={{textAlign: "left"}}>Message</TableHeaderColumn>,
@@ -19,11 +21,11 @@ class Changelog extends React.Component {
           ]}
           items={this.props.changelog.map((el, i) => {
             return (
-             <TableRow key={i} selectable={false}>
-                <TableRowColumn style={{textAlign: "left"}}>{el.message}</TableRowColumn>
-                <TableRowColumn style={{textAlign: "left", "width": "200px"}}>{prettyDate(el.time)}</TableRowColumn>
-                <TableRowColumn style={{textAlign: "left", "width": "200px"}}>{el.poster}</TableRowColumn>
-              </TableRow>
+             <tr key={i} className={s.row}>
+                <td className={cx(s.column, s.padded)}>{el.message}</td>
+                <td className={s.column} width="200px">{prettyDate(el.time)}</td>
+                <td className={s.column} width="200px">{el.poster}</td>
+              </tr>
             )
           })}
         />
