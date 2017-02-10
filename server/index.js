8fe82b0e9216505921fa2c5d03434002143baecd
Chris Hampu <chrishampu@outlook.com>
2017-02-09 17:26:01 -0800

    Further adjustment to caching. Use gzip properly. Remove koa-mount

--- server/index.js
+++ server/index.js
@@ -2,11 +2,9 @@
 import "babel-polyfill";
 import path from 'path';
 import Koa from 'koa';
-import mount from 'koa-mount';
 import convert from 'koa-convert';
 import body from 'koa-better-body';
 import cached from 'koa-static-cache';
-import compress from 'koa-compress';
 import historyApiFallback from 'koa-history-api-fallback';
 import chalk from 'chalk';
 import config from './config/environment';
@@ -14,20 +12,12 @@ import config from './config/environment';
 const httpServer = new Koa();
 
 httpServer.use(convert(historyApiFallback()));
-httpServer.use(compress());
 
-httpServer.use(mount('/bundle.js', cached(path.join(__dirname, '../build/bundle.js'), {
-  maxAge: 4 * 60 * 60,
+httpServer.use(cached(path.join(__dirname, '../build'), {
+  maxAge: 1 * 24 * 60 * 60,
   gzip: true,
   buffer: true,
   preload: true
-})));
-
-httpServer.use(mount('/', cached(path.join(__dirname, '../build'), {
-  maxAge: 365 * 24 * 60 * 60,
-  gzip: true,
-  buffer: true,
-  preload: true
-})));
+}));
 
 httpServer.listen(config.port, () => console.log(chalk.green(`HTTP server is listening on port ${config.port}`)));
