fefc1d5ab48e5504b83928b8f06e8092e6bad663
Chris Hampu <chrishampu@outlook.com>
2017-02-09 17:03:39 -0800

    Adjust server caching

--- server/index.js
+++ server/index.js
@@ -6,18 +6,28 @@ import mount from 'koa-mount';
 import convert from 'koa-convert';
 import body from 'koa-better-body';
 import cached from 'koa-static-cache';
+import compress from 'koa-compress';
 import historyApiFallback from 'koa-history-api-fallback';
 import chalk from 'chalk';
 import config from './config/environment';
+
 const httpServer = new Koa();
+
 httpServer.use(convert(historyApiFallback()));
+httpServer.use(compress());
 
 httpServer.use(mount('/bundle.js', cached(path.join(__dirname, '../build/bundle.js'), {
-  maxAge: 4 * 60 * 60
+  maxAge: 4 * 60 * 60,
+  gzip: true,
+  buffer: true,
+  preload: true
 })));
 
 httpServer.use(mount('/', cached(path.join(__dirname, '../build'), {
-  maxAge: 365 * 24 * 60 * 60
+  maxAge: 365 * 24 * 60 * 60,
+  gzip: true,
+  buffer: true,
+  preload: true
 })));
 
 httpServer.listen(config.port, () => console.log(chalk.green(`HTTP server is listening on port ${config.port}`)));
